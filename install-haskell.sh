d0ba36c2819712957eab5650f3118ebe666ebe2b
Maksim <zakharov@lumoza.ru>
2016-01-25 01:24:19 +0300

    Update to GHC 7.10

--- install-haskell.sh
+++ install-haskell.sh
@@ -1,15 +1,15 @@
 yum install tar bzip2 wget which -y
 cd /tmp
-wget -nv https://downloads.haskell.org/~ghc/7.8.3/ghc-7.8.3-$(uname -m)-unknown-linux-centos65.tar.bz2
+wget -nv https://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3-$(uname -m)-centos67-linux.tar.bz2
 yum install gcc perl -y
-tar -xf ghc-7.8.3-$(uname -m)-unknown-linux-centos65.tar.bz2
-cd ghc-7.8.3
+tar -xf ghc-7.10.3-$(uname -m)-centos67-linux.tar.bz2
+cd ghc-7.10.3
 ./configure && make install
 
 cd /tmp
-wget -nv https://www.haskell.org/cabal/release/cabal-install-1.20.0.6/cabal-install-1.20.0.6.tar.gz
-tar -xf cabal-install-1.20.0.6.tar.gz
-cd cabal-install-1.20.0.6
+wget -nv https://www.haskell.org/cabal/release/cabal-install-1.22.7.0/cabal-install-1.22.7.0.tar.gz
+tar -xf cabal-install-1.22.7.0.tar.gz
+cd cabal-install-1.22.7.0
 yum install gmp-static zlib-devel -y
 ./bootstrap.sh
 export PATH=~/.cabal/bin:$PATH

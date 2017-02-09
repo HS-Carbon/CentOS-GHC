b9f8986b45d0e6f37c9a2a9900df707c5cb60599
Maksim <zakharov@lumoza.ru>
2017-02-09 22:02:00 +0300

    Migrate to Stack

--- install-haskell.sh
+++ install-haskell.sh
@@ -1,20 +1,5 @@
-yum install tar bzip2 wget which -y
-cd /tmp
-wget -nv https://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3-$(uname -m)-centos67-linux.tar.bz2
-yum install gcc glibc-static perl -y
-tar -xf ghc-7.10.3-$(uname -m)-centos67-linux.tar.bz2
-cd ghc-7.10.3
-./configure && make install
-
-cd /tmp
-wget -nv https://www.haskell.org/cabal/release/cabal-install-1.22.7.0/cabal-install-1.22.7.0.tar.gz
-tar -xf cabal-install-1.22.7.0.tar.gz
-cd cabal-install-1.22.7.0
-yum install gmp-static zlib-devel -y
-./bootstrap.sh
-export PATH=~/.cabal/bin:$PATH
-cabal update
-if ! grep "split-objs: True" ~/.cabal/config; then echo "split-objs: True" >> ~/.cabal/config; fi
-cabal install alex happy
+curl -sSL https://get.haskellstack.org/ | sh
+yum install gmp-static -y
 
 rm -rf /tmp/*
+
